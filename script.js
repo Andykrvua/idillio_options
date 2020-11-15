ba3323cf825419ced16c1ff33410730e2c5475c8
Andrey <andrey.kallko@gmail.com>
2020-11-15 21:33:58 +0200

    work

--- script.js
+++ script.js
@@ -35,8 +35,94 @@ function sizeBlockf() {
   for (var item of items) {
     item.style.width = elemWidth + 'px';
   }
-  sort(items);
-  missingItems(items);
+  // sort(items);
+
+  // add row size
+  let arr = [];
+  for (var item of items) {
+    arr.push(item.dataset.size);
+  }
+  arrUnicSize = Array.from(new Set(arr));
+  let arrUnicSizeCount = 0;
+  for (var item of items) {
+    if (item.dataset.size == arrUnicSize[arrUnicSizeCount]) {
+    } else {
+      let parentDiv = item.parentNode;
+      parentDiv.insertBefore(createElem(arrUnicSize[arrUnicSizeCount]), item);
+      arrUnicSizeCount++;
+    }
+  }
+  let parentDiv = sizeBlock.querySelector('div[id|="input"]');
+  parentDiv.insertBefore(createElem(arrUnicSize[arrUnicSize.length - 1]), null);
+
+  addWoodDataCount(items);
+
+  insertMissingElems(items);
+}
+
+function insertMissingElems(items) {
+  let newSizeBlock = document.querySelector('[option$="15"]');
+  let newItems = newSizeBlock.querySelectorAll('.product-page__radio-box');
+  let missingArr = checkMissingElems(items);
+  console.log(missingArr);
+  let unicWood = countColRow(items, 'wood');
+  for (var i = 0; i < missingArr.length; i++) {
+    if (missingArr[i][1] != unicWood) {
+      for (var item of newItems) {
+        // console.log(item);
+        if (item.dataset.woodrow == missingArr[i][0]) {
+          let missing = unicWood - missingArr[i][1];
+          console.log('в размере ' + missingArr[i][0] + ' ' + missing + 'эл');
+          for (var t = 0; t < missing; t++) {
+            let parentDiv = item.parentNode;
+            parentDiv.insertBefore(
+              createElem('placeholder', '100px', '999', missingArr[i][0]),
+              item
+            );
+          }
+        }
+      }
+    }
+  }
+  addWoodCountPlaceholder(missingArr, unicWood);
+}
+
+//add data-wood count placeholder items
+function addWoodCountPlaceholder(missingArr, unicWood) {
+  let newSizeBlock = document.querySelector('[option$="15"]');
+  let newItems = newSizeBlock.querySelectorAll('.product-page__radio-box');
+  // return arr all size ["1200х800х750", 6]
+  console.log(missingArr);
+  console.log(unicWood);
+  arr = [];
+  for (var i = 0; i < missingArr.length; i++) {
+    for (var item of newItems) {
+      if (item.dataset.woodrow === missingArr[i][0]) {
+        arr = [];
+      }
+      if (item.dataset.size === missingArr[i][0]) {
+        let missing = unicWood - missingArr[i][1];
+        if (missing) {
+          if (item.dataset.pos != 999) {
+            arr.push(item.dataset.pos);
+            console.log('arr1 = ' + arr);
+          } else {
+            for (var g = 1; g <= unicWood; g++) {
+              if (arr.indexOf(String(g)) == -1) {
+                item.setAttribute('data-pos', g);
+                arr.push(String(g));
+                console.log('arr2 = ' + arr);
+                debugger;
+                break;
+              }
+              // console.log('arr2 = ' + arr);
+            }
+          }
+          console.log('miss = ' + missing + ' ' + item.dataset.size);
+        }
+      }
+    }
+  }
 }
 
 //count unic size and wood = quantity columns and rows
@@ -49,36 +135,37 @@ function countColRow(items, data) {
 }
 
 //sort
-function sort(nodeList) {
-  let sizeBlock = document.querySelector('[option$="15"]');
-  var itemsArray = [];
-  for (var i = 0; i < nodeList.length; i++) {
-    itemsArray.push(nodeList[i]);
-  }
-  // console.dir('NodeList' + itemsArray);
-  itemsArray
-    .sort(function (nodeA, nodeB) {
-      var textA = nodeA.dataset.size;
-      var textB = nodeB.dataset.size;
-      var numberA = parseInt(textA);
-      var numberB = parseInt(textB);
-      if (numberA < numberB) return -1;
-      if (numberA > numberB) return 1;
-      return 0;
-    })
-    .forEach(function (node) {
-      sizeBlock.appendChild(node);
-    });
-}
+// function sort(nodeList) {
+//   let sizeBlock = document.querySelector('[option$="15"]');
+//   var itemsArray = [];
+//   for (var i = 0; i < nodeList.length; i++) {
+//     itemsArray.push(nodeList[i]);
+//   }
+//   console.dir('NodeList' + itemsArray);
+//   itemsArray
+//     .sort(function (nodeA, nodeB) {
+//       var textA = nodeA.dataset.size;
+//       var textB = nodeB.dataset.size;
+//       var numberA = parseInt(textA);
+//       var numberB = parseInt(textB);
+//       if (numberA < numberB) return -1;
+//       if (numberA > numberB) return 1;
+//       return 0;
+//     })
+//     .forEach(function (node) {
+//       sizeBlock.appendChild(node);
+//     });
+// }
 
-//missing items
-function missingItems(items) {
+// add iterate data attr wood
+function addWoodDataCount(items) {
   let arrUnicWood = countColRow(items, 'wood');
 
   let arr = [];
   for (var item of items) {
     arr.push(item.dataset.size);
   }
+
   arrUnicSize = Array.from(new Set(arr));
 
   let arrUnicSizePos = [];
@@ -101,26 +188,49 @@ function missingItems(items) {
   }
   console.log('unic: ' + arrUnicSizePos);
 
-  // arrUnicWood = Array.from(new Set(arrUnicWood));
-  // arrUnicSize = Array.from(new Set(arrUnicSize));
-  // console.log(arrUnicWood);
-  // console.log(arrUnicSize);
+  // set attr data-pos
+  for (var i = 0; i < arrUnicSizePos.length; i++) {
+    for (var item of items) {
+      if (item.dataset.wood === arrUnicSizePos[i]) {
+        item.setAttribute('data-pos', i + 1);
+      }
+    }
+  }
+}
 
-  // for (var i = 0; i < 6; i++) {
-  //   if (items[i].dataset.wood === arrUnicWood[i]) {
-  //     console.log('done');
-  //     console.log('done' + items[i].dataset.wood);
-  //     console.log('done' + arrUnicWood[i]);
-  //   } else {
-  //     let newItem = document.createElement('DIV');
-  //     newItem.classList.add('product-page__radio-box');
-  //     let textnode = document.createTextNode('Test');
-  //     newItem.appendChild(textnode);
-  //     let parentDiv = items[i].parentNode;
-  //     parentDiv.insertBefore(newItem, items[i]);
-  //     console.log('fuck');
-  //   }
-  // }
+function checkMissingElems(items) {
+  let arr = [];
+  let count = 0;
+  for (var i = 0; i < arrUnicSize.length; i++) {
+    for (var item of items) {
+      if (item.dataset.size === arrUnicSize[i]) {
+        ++count;
+      }
+    }
+    arr.push([arrUnicSize[i], count]);
+    count = 0;
+  }
+  return arr;
+}
+
+function createElem(
+  name = 'test',
+  width = '100%',
+  dataPos = false,
+  size = false
+) {
+  let newItem = document.createElement('DIV');
+  newItem.classList.add('product-page__radio-box');
+  let textnode = document.createTextNode(name);
+  newItem.appendChild(textnode);
+  newItem.setAttribute('data-woodrow', name);
+  newItem.style.width = width;
+  if (dataPos && size) {
+    newItem.setAttribute('data-pos', dataPos);
+    newItem.setAttribute('data-size', size);
+  }
+
+  return newItem;
 }
 
 //window resize
